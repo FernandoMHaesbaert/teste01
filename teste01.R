2739d871049414dd042a98a71c4e4b3d1e83ed40
FernandoMHaesbaert <fhaesbaert@gmail.com>
2025-11-13 10:07:13 -0300

    segundo commit

--- teste01.R
+++ teste01.R
@@ -1,1 +1,9 @@
+# Criar um print com a fras
 print("Hello world")
+# criar dataframe com duas variaveis e 5 observações
+df <- data.frame(
+  var1 = c(1, 2, 3, 4, 5),
+  var2 = c("A", "B", "C", "D", "E")
+)
+# mostrar o dataframe
+print(df)
